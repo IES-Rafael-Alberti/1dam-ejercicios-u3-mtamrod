be5d78a86b195613091fbab5e2c0b4d9a2c11985
mtamrod <trmarioiesrafaelalberti.es>
2023-11-17 01:11:54 +0100

    subida

--- src/ej3_01.py
+++ src/ej3_01.py
@@ -3,15 +3,12 @@ def almacenar_asig(n_asig) -> list:
     for i in range (n_asig):
         asignatura = str(input("Introduce la asignatura: "))
         asignaturas.append(asignatura)
-    recorrer_asig(asignaturas)
-
-def recorrer_asig(asignaturas):
-    for asignatura in asignaturas:
-        print(f"Yo estudio {asignatura}.")
+    return print(asignaturas)
 
 def main():
     n_asig = int(input("¿Cuántas asignaturas quiere introducir?: "))
     almacenar_asig(n_asig)
     
+      
 if __name__ == "__main__":
     main()
--- src/ej3_02.py
+++ src/ej3_02.py
@@ -1,1 +1,17 @@
+def almacenar_asig(n_asig) -> list:
+    asignaturas = []
+    for i in range (n_asig):
+        asignatura = str(input("Introduce la asignatura: "))
+        asignaturas.append(asignatura)
+    recorrer_asig(asignaturas)
 
+def recorrer_asig(asignaturas):
+    for asignatura in asignaturas:
+        print(f"Yo estudio {asignatura}.")
+
+def main():
+    n_asig = int(input("¿Cuántas asignaturas quiere introducir?: "))
+    almacenar_asig(n_asig)
+    
+if __name__ == "__main__":
+    main()
